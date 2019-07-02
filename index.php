bac22239d9694a2b597fae577be7d18757a38fca
Jati Pikukuh <jati.pikukuh@mail.ugm.ac.id>
2019-07-02 15:19:18 +0700

    add php

--- index.php
+++ index.php
@@ -1,9 +1,13 @@
+<?php
+    $hello = 'Hello world!';
+?>
+
 <html>
     <head>
         <title>Hello world</title>
     </head>
     <body>
-        <h1>Hello world!</h1>
+        <h1><?php echo $hello; ?></h1>
         <h3>Welcome to VSGA 2019</h3>
         <h5>Lorem Ipsum dolor sit amet</h5>
     </body>
